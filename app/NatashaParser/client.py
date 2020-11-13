7706ebd7c1cdc94f83634c5636bf2c89b95c6419
IstackShabarshin <69294313+IstackShabarshin@users.noreply.github.com>
2020-11-13 11:42:44 +0300

    create and reform module NatashaParser

--- app/NatashaParser/client.py
+++ app/NatashaParser/client.py
@@ -1,36 +1,26 @@
-
 import os
 import socket
 
 SOCKET_FILE = './tmp/natasha.socket'
 
-print("Connecting...")
-if os.path.exists(SOCKET_FILE):
-    client1 = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+def conn_natasha(string, param)
+    print("Connecting...")
+    if os.path.exists(SOCKET_FILE):
+        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-    client1.connect(SOCKET_FILE)
-    print("Connected")
-    print("Ctrl-C to close")
-    print("Send 'DONE' to stop server")
-    try:
-        x = input("> ")
-        if "" != x:
-            print("SENDED: %s" % x)
-            client1.send(x.encode())
+        client.connect(SOCKET_FILE)
+        try:
+            client.send(string.encode())
+            client.send(param.encode())
 
             result = []
-            resp = client1.recv(1024).decode()
+            resp = client.recv(1024).decode()
             result.append(resp)
             while (len(resp) > 0):
-                resp = client1.recv(1024).decode()
+                resp = client.recv(1024).decode()
                 result.append(resp)
-
-            print(result)
-
-    except KeyboardInterrupt as k:
-        print("STOPPING client")
-    finally:
-        client1.close()
-else:
-    print("Can't connect to server")
-print("Executed")
+        finally:
+            client.close()
+            return result
+    else:
+        raise FileNotFoundError(SOCKET_FILE)
--- app/NatashaParser/main.py
+++ app/NatashaParser/main.py
@@ -1,33 +1,79 @@
 import os, socket
-import NatashaParser
+from NatashaParser.parser import (
+    FindNames,
+    FindDates,
+    FindAddrs,
+    Normalize
+)
 
 SOCKET_FILE = './tmp/natasha.socket'
 
-if os.path.exists(SOCKET_FILE):
-    os.remove(SOCKET_FILE)
+def conn_natasha(string, param):
+    print("Connecting...")
+    if os.path.exists(SOCKET_FILE):
+        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-print("Open UNIX socket")
-server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-server.bind(SOCKET_FILE)
-server.listen(1)
+        client.connect(SOCKET_FILE)
+        try:
+            client.send(string.encode())
+            client.send(param.encode())
 
-print("Licenning...")
-#conn, _ = server.accept()
-while True:
-    conn, _ = server.accept()
-    datagram = conn.recv(1024).decode()
-    if not datagram:
-        break
+            result = []
+            resp = client.recv(1024).decode()
+            result.append(resp)
+            while (len(resp) > 0):
+                resp = client.recv(1024).decode()
+                result.append(resp)
+        finally:
+            client.close()
+            return result
     else:
-        print("-" * 20, flush=True)
-    print(datagram, flush=True)
-    request = NatashaParser.Normalize(datagram)
-    print(request)
-    for elem in request:
-        conn.send(elem.encode())
-    conn.close()
+        raise FileNotFoundError(SOCKET_FILE)
 
-print("Closing...");
-server.close()
-os.remove(SOCKET_FILE)
-print("Closed")
+def main():
+    if os.path.exists(SOCKET_FILE):
+        os.remove(SOCKET_FILE)
+
+    print("Open UNIX socket", flush=True)
+    server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    server.bind(SOCKET_FILE)
+    server.listen(1)
+
+    print("Licenning...")
+    try:
+        while True:
+            try:
+                print("Open new connect")
+                conn, _ = server.accept()
+                string = conn.recv(1024).decode()
+                param = conn.recv(1024).decode()
+                if not string or not param:
+                    break
+                else:
+                    raise ValueError(string)
+                
+                #param parsing
+                if param == '-n': #Normalize
+                    request = Normalize(string)
+                elif param == '-fn': #FindNames
+                    request = FindNames(string)
+                elif param == '-fd': #FindDates
+                    request = FindDates(string)
+                elif param == '-fa': #FindAddrs
+                    request = FindAddrs(string)
+                else:
+                    raise ValueError(param)
+                
+                for elem in request:
+                    conn.send(elem.encode())
+            finally:
+                print("Close connect")
+                conn.close()
+    finally:
+        print("Closing server...");
+        server.close()
+        os.remove(SOCKET_FILE)
+        print("Closed")
+
+if __name__ == "__main__":
+    main()
